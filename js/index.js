d8d1771feb9a22ebabc6e00961b5416b6561d530
fez994 <dentefederico@live.com>
2016-03-28 15:28:06 +0200

    Aggiunta sezione About me, scroll down div con relativo effetto su jquery, font nuovi sia all'header che al menù

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-/*
-* Uses jQuery!!!!111one
-*/
-
-
-/*====================================
-=            ON DOM READY            =
-====================================*/
 $(function() {
   
-    // Toggle Nav on Click
+    
     $('.toggle-nav').click(function() {
-        // Calling a function in case you want to expand upon this.
+        
         toggleNav();
     });
 
@@ -18,17 +10,24 @@ $(function() {
 });
 
 
-/*========================================
-=            CUSTOM FUNCTIONS            =
-========================================*/
+
 function toggleNav() {
     if ($('#site-wrapper').hasClass('show-nav')) {
-        // Do things on Nav Close
+        
         $('#site-wrapper').removeClass('show-nav');
     } else {
-        // Do things on Nav Open
+        
         $('#site-wrapper').addClass('show-nav');
     }
 
     //$('#site-wrapper').toggleClass('show-nav');
 }
+
+
+$(document).ready(function (){
+    $('#arrowcontainer').click(function(){
+        $("html, body").animate ({
+            scrollTop: $("#about").offset().top
+        }, 'slow');
+    });
+});
